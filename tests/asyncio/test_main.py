1e5f6d15c13cd699c0474a04e0d00ce0d523b27c
Mustafa Mohamed <git@ms7m.me>
2022-10-27 03:56:01 -0400

    fix: update tests

--- tests/asyncio/test_main.py
+++ tests/asyncio/test_main.py
@@ -2,12 +2,22 @@
 
 from ridant.asyncio.main import RidantCache
 from pydantic import BaseModel
-
+import pytest
+from redis.asyncio import Redis
 
 class SamplePydanticModel(BaseModel):        
     name: str
     age: int
     
+async def test_object_initialization(return_connection_pool_for_async_redis):
+    cache_object_no_hash = RidantCache(redis_connection_pool=return_connection_pool_for_async_redis)
+    with pytest.raises(ValueError):
+        cache_object_no_hash.redis_hashed
+        
+    cache_object_hash = RidantCache(redis_connection_pool=return_connection_pool_for_async_redis, redis_database_for_hash=1)
+    assert isinstance(cache_object_hash.redis, Redis)
+    assert isinstance(cache_object_hash.redis_hashed, Redis)
+    assert isinstance(cache_object_no_hash.redis, Redis)
 
 
 async def test_cache_item(return_connection_pool_for_async_redis):
@@ -24,13 +34,13 @@ async def test_delete_cache_item(return_connection_pool_for_async_redis):
     
     
 async def test_hash_set_cache_item(return_connection_pool_for_async_redis):
-    cache = RidantCache(redis_connection_pool=return_connection_pool_for_async_redis)
+    cache = RidantCache(redis_connection_pool=return_connection_pool_for_async_redis, redis_database_for_hash=1)
     await cache.cache(SamplePydanticModel(name="test", age=1), "test", hash=True)
     await cache.find_one(SamplePydanticModel, "test", "name") == "test"
     await cache.find_one(SamplePydanticModel, "test", "age") == 1
 
 async def test_hash_update_cache_item(return_connection_pool_for_async_redis):
-    cache = RidantCache(redis_connection_pool=return_connection_pool_for_async_redis)
+    cache = RidantCache(redis_connection_pool=return_connection_pool_for_async_redis, redis_database_for_hash=1 )
     await cache.cache(SamplePydanticModel(name="test", age=1), "test", hash=True)
     await cache.update(SamplePydanticModel, "test", "age", 2)
     await cache.find_one(SamplePydanticModel, "test", "name") == "test"
--- tests/fixtures/redis_connection.py
+++ tests/fixtures/redis_connection.py
@@ -1,15 +1,17 @@
 from redis.asyncio.connection import ConnectionPool as AsyncConnectionPool
 from redis.connection import ConnectionPool as SyncConnectionPool
-import pytest
+import pytest, os
 
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 
 @pytest.fixture
 async def return_connection_pool_for_async_redis():
-    conn = AsyncConnectionPool(host="localhost", port=6379, db=0)
+    conn = AsyncConnectionPool(host=REDIS_HOST, port=REDIS_PORT, db=0)
     yield conn
 
 @pytest.fixture
 def return_connection_pool_for_sync_redis():
-    conn = SyncConnectionPool(host="localhost", port=6379, db=0)
+    conn = SyncConnectionPool(host=REDIS_HOST, port=REDIS_PORT, db=0)
     yield conn
 
